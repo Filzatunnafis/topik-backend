5461b1d5aae4b73d17f2cbc27683e2517b128c5c
Filzatunnafis <filzatunnafisd@gmail.com>
2025-12-21 14:08:42 +0800

    Fix Supabase pooler timeout (disable prepared statements)

--- db/pool.js
+++ db/pool.js
@@ -2,7 +2,15 @@ const { Pool } = require("pg");
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false }
+  ssl: { rejectUnauthorized: false },
+
+  max: 5,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 10000,
+  statement_timeout: 10000,
+  query_timeout: 10000,
+
+  prepareThreshold: 0
 });
 
 module.exports = pool;
