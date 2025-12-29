44cf9cab6570f854efcc64633991af8999ceb13c
Filzatunnafis <filzatunnafisd@gmail.com>
2025-12-29 23:30:11 +0800

    Fix SSL self-signed certificate chain issue

--- db/pool.js
+++ db/pool.js
@@ -1,12 +1,17 @@
 const { Pool } = require("pg");
 
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false },
 
-  max: 5,                     
+  max: 5,
   idleTimeoutMillis: 30000,
-  connectionTimeoutMillis: 20000
+  connectionTimeoutMillis: 20000,
+
+  statement_timeout: 0,
+  query_timeout: 0
 });
 
 module.exports = pool;
