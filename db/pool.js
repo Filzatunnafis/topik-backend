4217671ffbefda11f8ade0f67260234a6b87abbe
Filzatunnafis <filzatunnafisd@gmail.com>
2025-12-29 18:07:13 +0800

    Limit PostgreSQL pool connections for free-tier stability

--- db/pool.js
+++ db/pool.js
@@ -4,9 +4,9 @@ const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false },
 
-  max: 5,
-  idleTimeoutMillis: 30000,
-  connectionTimeoutMillis: 10000,
+  max: 1,                     
+  connectionTimeoutMillis: 15000,
+
   statement_timeout: 10000,
   query_timeout: 10000,
 
