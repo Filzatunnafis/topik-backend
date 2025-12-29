168bd52f222e77c0a02c4090b8dc819beaa2b7bf
Filzatunnafis <filzatunnafisd@gmail.com>
2025-12-29 23:35:39 +0800

    Fix TLS env order before pg import

--- db/pool.js
+++ db/pool.js
@@ -1,6 +1,6 @@
-const { Pool } = require("pg");
-
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+
+const { Pool } = require("pg");
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
